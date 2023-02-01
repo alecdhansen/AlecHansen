e6300a5e173b8960360ef430229058269a6b6703
Alec Hansen <alecdhansen@gmail.com>
2023-02-01 16:32:58 -0500

    adjust icons

---
+++ src/components/icons/Dot.tsx
@@ -0,0 +1,33 @@
+const DotIcon = () => {
+  return (
+    <svg
+      width="225px"
+      height="225px"
+      viewBox="-2.4 -2.4 28.80 28.80"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0">
+        <path
+          transform="translate(-2.4, -2.4), scale(1.7999999999999998)"
+          fill="url(#grad0)"
+          d="M9.166.33a2.25 2.25 0 00-2.332 0l-5.25 3.182A2.25 2.25 0 00.5 5.436v5.128a2.25 2.25 0 001.084 1.924l5.25 3.182a2.25 2.25 0 002.332 0l5.25-3.182a2.25 2.25 0 001.084-1.924V5.436a2.25 2.25 0 00-1.084-1.924L9.166.33z"
+          stroke-width="0"
+        ></path>
+      </g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M12 9.5C13.3807 9.5 14.5 10.6193 14.5 12C14.5 13.3807 13.3807 14.5 12 14.5C10.6193 14.5 9.5 13.3807 9.5 12C9.5 10.6193 10.6193 9.5 12 9.5Z"
+          fill="#000000"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+};
+export default DotIcon;
