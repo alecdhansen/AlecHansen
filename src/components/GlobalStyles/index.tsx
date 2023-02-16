cf93a8b163819be2f0d4c4c9f563b953e72c6d28
Alec Hansen <alecdhansen@gmail.com>
2023-02-16 13:07:11 -0500

    add global styles file

--- src/components/GlobalStyles/index.tsx
+++ src/components/GlobalStyles/index.tsx
@@ -20,17 +20,56 @@ export const GlobalStyles = createGlobalStyle<{ theme: any }>`
     transition: all 0.50s linear;
   }
 
+  body {
+  margin: 0;
+  display: flex;
+  place-items: center;
+  min-width: 320px;
+  min-height: 100vh;
+}
+
+h1 {
+  font-size: 3.2em;
+  line-height: 1.1;
+}
+
+h2 {
+  color: ${({ theme }) => theme.color};
+  transition: all 0.50s linear;
+}
+
+span {
+  transition: all 0.50s linear;
+}
+
+button {
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: var(--dark);
+  cursor: pointer;
+}
+button:hover {
+  border-color: #646cff;
+}
+button:focus,
+button:focus-visible {
+  outline: 4px auto -webkit-focus-ring-color;
+}
+
 a {
-    /* background: ${({ theme }) => theme.backgroundColor}; */
     color: ${({ theme }) => theme.color};
     transition: all 0.50s linear;
+    font-weight: 500;
+    text-decoration: inherit;
 }
 
 a:hover {
     transition: all 0.10s linear;
+    color: ${({ theme }) => theme.textHoverColor};
 }
 
-  a:after {
-  
-  }
   `;
