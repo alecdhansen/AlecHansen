6e3e4a1c6999b724927edee024659ab317c5688d
Alec Hansen <alecdhansen@gmail.com>
2023-02-17 17:58:14 -0500

    tweak lightbulb svgs

--- src/components/icons/LightbulbFilled.tsx
+++ src/components/icons/LightbulbFilled.tsx
@@ -1,27 +1,32 @@
-const LightbulbFilled = () => {
+const LightbulbFilled = (colorTheme?: any) => {
   return (
     <svg
-      fill="#000000"
-      width="40px"
-      height="40px"
-      viewBox="-0.48 -0.48 24.96 24.96"
+      width="42px"
+      height="42px"
+      viewBox="-1.2 -1.2 26.40 27.40"
       xmlns="http://www.w3.org/2000/svg"
+      strokeWidth="0.00024000000000000003"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0">
         <path
-          transform="translate(-0.48, -0.48), scale(1.56)"
+          transform="translate(-1.2, -1.2), scale(1.65)"
           fill="url(#grad0)"
           d="M9.166.33a2.25 2.25 0 00-2.332 0l-5.25 3.182A2.25 2.25 0 00.5 5.436v5.128a2.25 2.25 0 001.084 1.924l5.25 3.182a2.25 2.25 0 002.332 0l5.25-3.182a2.25 2.25 0 001.084-1.924V5.436a2.25 2.25 0 00-1.084-1.924L9.166.33z"
           strokeWidth="0"
-        ></path>
+        />
       </g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      ></g>
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
       <g id="SVGRepo_iconCarrier">
-        <path d="M15,16.372a7.5,7.5,0,1,0-6,0V18h6ZM11.221,6.087a3.5,3.5,0,0,0-2.7,3.023,1,1,0,0,1-.992.89,1.065,1.065,0,0,1-.112-.006,1,1,0,0,1-.884-1.1,5.5,5.5,0,0,1,4.246-4.754,1,1,0,0,1,.442,1.951ZM9,20h6v1a1,1,0,0,1-1,1H10a1,1,0,0,1-1-1Z"></path>
+        <path
+          fill={
+            colorTheme.colorTheme === "light" ? "#fad759" : "rgb(42, 39, 39)"
+          }
+          d="M15,16.372a7.5,7.5,0,1,0-6,0V18h6ZM11.221,6.087a3.5,3.5,0,0,0-2.7,3.023,1,1,0,0,1-.992.89,1.065,1.065,0,0,1-.112-.006,1,1,0,0,1-.884-1.1,5.5,5.5,0,0,1,4.246-4.754,1,1,0,0,1,.442,1.951ZM9,20h6v1a1,1,0,0,1-1,1H10a1,1,0,0,1-1-1Z"
+        ></path>
       </g>
     </svg>
   );
